d5b2de1d67b8b004b4e43856f892d1e4a4dd74a9
Macho <155906434+machoLTD@users.noreply.github.com>
2024-10-24 13:32:30 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'https://github.com/machoLTD'
+description 'Simple Teleport 2 Waypoint Script '
+version '1.0.0'
+
+-- What to run
+client_scripts {
+    'teleport2waypoint.lua',
+   
+}
